0cfa4c86f1b155500cf2f465900a15bc06db2ae8
Victor Villazón <victorzn23@gmail.com>
2023-12-18 23:03:48 -0500

    03-javascript-es6-fetch-countries

--- javascript-es6-fetch-countries/main.js
+++ javascript-es6-fetch-countries/main.js
@@ -1,6 +1,39 @@
 // const url = 'https://restcountries.com/v3.1/all'
 const url = 'https://restcountries.com/v3.1/all?fields=name,flags,region,capital,population,languages,currencies,timezones'
 
+const formatNumber = (number) => {
+  return new Intl.NumberFormat('es-PE').format(number)
+}
+
+const renderCountries = (countries) => {
+  const countryListElement = document.querySelector('.app__list')
+
+  let countryList = ''
+
+  countries.forEach(country => {
+    // console.log(country)
+    countryList += `
+      <div class="country">
+        <img class="country__flag" src="${country.flags.svg}" alt="${country.name.common}" />
+        <div class="country__wrapper">
+          <h2 class="country__title">${country.name.common}</h2>
+          <div class="country__description">
+            <strong>Population:</strong> ${formatNumber(country.population)}
+          </div>
+          <div class="country__description">
+            <strong>Region:</strong> ${country.region}
+          </div>
+          <div class="country__description">
+            <strong>Capital:</strong> ${country.capital}
+          </div>
+        </div>
+      </div>
+    `
+  })
+
+  countryListElement.innerHTML = countryList
+}
+
 const fetchCountries = async () => { // Devuelve una Promesa (Promise)
   try {
     const response = await fetch(url)
@@ -16,4 +49,4 @@ const fetchCountries = async () => { // Devuelve una Promesa (Promise)
 }
 
 fetchCountries()
-  .then(data => console.log(data))
+  .then(data => renderCountries(data))
