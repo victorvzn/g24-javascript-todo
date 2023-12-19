a438f877693ccc12480f8f297141b85307e7e58b
Victor Villazón <victorzn23@gmail.com>
2023-12-18 19:54:37 -0500

    01-javascript-todo

--- javascript-todo/main.js
+++ javascript-todo/main.js
@@ -1,1 +1,24 @@
-console.log('Hola Javascript')
+// console.log('Hola Javascript')
+
+// const taskInput = document.getElementById('taskInput')
+const taskInput = document.querySelector('.task__input')
+const taskAdd = document.querySelector('.task__add')
+const taskList = document.querySelector('.task__list')
+
+taskAdd.addEventListener('click', function (event) {
+  // ¡La lógica después de hacer click!
+  // Se ejecutará cuando hagamos click en el botón "Añadir tarea"
+
+  console.log('Hice click!')
+
+  const button = document.createElement('button')
+  button.innerText = 'Hola soy un botón dinámico!'
+  document.body.appendChild(button)
+
+  
+})
+
+console.log(taskInput)
+console.log(taskInput.placeholder)
+console.log(taskAdd)
+console.log(taskList)
