b20ed9496d42d02c631627ce889587ecac84553a
Victor Villazón <victorzn23@gmail.com>
2023-12-19 23:04:51 -0500

    04-javascript-es6-fetch-pagination

--- javascript-es6-fetch-pagination/main.js
+++ javascript-es6-fetch-pagination/main.js
@@ -1,6 +1,8 @@
 // console.log('Hola JS')
 let currentPage = 1
 
+let pokemonFavorites = []
+
 const paginationPrev = document.querySelector('.pagination__prev')
 const paginationNext = document.querySelector('.pagination__next')
 
@@ -11,14 +13,14 @@ paginationNext.addEventListener('click', (event) => {
     .then(data => renderCharacters(data.results))
 })
 
+// TODO: añadir el botón 'previous' para regresar en la paginación
+
 paginationPrev.addEventListener('click', (event) => {
   currentPage -= 1
 
   fetchCharacters(currentPage)
     .then(data => renderCharacters(data.results))
 })
-
-// TODO: añadir el botón 'previous' para regresar en la paginación
 
 const renderCharacters = (characters) => {
   const appCharacters = document.querySelector('.app__characters')
@@ -30,11 +32,33 @@ const renderCharacters = (characters) => {
       <div class="app__character">
         <img src="${character.image}" width="200" />
         <h2>${character.id}: ${character.name}</h2>
+        <button
+          class="character__button"
+          data-id="${character.id}"
+          data-name="${character.name}">
+            ❤ Add to favorites
+        </button>
       </div>
     `
   })
 
   appCharacters.innerHTML = charactersList
+
+  const favoriteButtons = document.querySelectorAll('.character__button')
+
+  favoriteButtons.forEach(button => {
+    button.addEventListener('click', (event) => {
+      // console.log('Click on favorite button!')
+      const { id, name } = event.target.dataset
+
+      pokemonFavorites.push({
+        id,
+        name
+      })
+
+      console.log(pokemonFavorites)
+    })
+  })
 }
 
 const fetchCharacters = async function (page = 1) {
